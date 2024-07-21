b3b1b4b47cd25c4a9f477a5199cd829cff1a8b67
manoyfinaritra <manoyfinaritra@gmail.com>
2024-07-21 11:43:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form"
+function App(){
+    return(
+        <>
+        <div className="container-manoy">
+            <Form />
+        </div>
+        </>
+    )
+
+}
+export default App
